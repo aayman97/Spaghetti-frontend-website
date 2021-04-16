b7ec1a1fe63ae588dafba2ba440506fffabca2df
Ahmed Ayman <36381734+aayman97@users.noreply.github.com>
2021-04-16 20:47:11 +0200

    converting a actual design into a frontend website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import menu from './images/menu.png'
+import spaghetti from './images/spaghetti.png'
+import facebook from './images/fb.png'
+import instagram from './images/instagram.png'
+import twitter from './images/twitter.png'
+import youtube from './images/youtube.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="navbar">
+          <h1>Foodies</h1>
+          <div className="rightSideContainer">
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+          <img src={menu} className="menuLogo"></img>
+          </div>
+        </div>
+
+        <div className="spaghettiSection">
+          <div className="leftside">
+              <h1>Spaghetti</h1>
+              <p>Spaghetti and meatballs or Spaghetti with meatballs ia an italian
+                -American dish consisting of spaghetti, tomato sauce and meatballs
+              </p>
+              <button>Order your food</button>
+          </div>
+          <div className="rigthside">
+            <img src={spaghetti} className="spaghetti"/>
+          </div>  
+        </div>
+
+        <div className="socialContainer">
+            <img src={facebook} className="socialmedialogo"/>
+            <img src={twitter} className="socialmedialogo"/>
+            <img src={instagram} className="socialmedialogo"/>
+            <img src={youtube} className="socialmedialogo"/>
+        </div>
     </div>
   );
 }
